6dd103ece1b697d87185892c9e2f6f027adbacaa
maheshmba <maheshmba>
2023-11-20 01:13:08 +0000

    test

---
+++ firstnb.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+message = 'Welcome to Databricks'
+
+# COMMAND ----------
+
+print(message)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "Hello"
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
